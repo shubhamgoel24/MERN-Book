4eaf1eb8605d831f18f7c578aa57ae5bc793b192
Shubham Goel <sshubhamgoel24@gmail.com>
2021-08-26 20:01:00 +0530

    basic user create

--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,9 @@ const router = express.Router();
 const usercontroller = require('../controllers/users_controller');
 
 router.get('/profile', usercontroller.profile);
+router.get('/sign-in', usercontroller.signIn);
+router.get('/sign-up', usercontroller.signUp);
+router.post('/create', usercontroller.create);
+
 
 module.exports = router;
