2d2462fdba19c261f24106e16a433d2fa4c75cbb
Shubham Goel <sshubhamgoel24@gmail.com>
2021-09-03 09:39:26 +0530

    Added async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,23 +1,24 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home = function(req,res){
-    Post.find({})
-    .populate('user')
-    .populate({
-        path:'comments',
-        populate:{
-            path: 'user'
-        }
-    })
-    .exec(function(err,posts){
-        User.find({}, function(err, users){
-            return res.render('home', {
-                posts : posts,
-                all_users : users
-            });
+module.exports.home = async function(req,res){
+    try{
+        //populate the user of each post
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path:'comments',
+            populate:{
+                path: 'user'
+            }
         });
-        
-    })
-    
+        let users = await User.find({});
+        return res.render('home', {
+            posts : posts,
+            all_users : users
+        });
+    }catch(err){
+        console.log("Error:", err);
+        return;
+    }     
 }
