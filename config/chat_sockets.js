b48e4d6590aa8eb0e80012b84e629e2c696b57c0
Shubham Goel <sshubhamgoel24@gmail.com>
2021-11-02 13:11:20 +0530

    Websocket fix

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -3,7 +3,7 @@ module.exports.chatSockets = function(socketServer){
     const io = require("socket.io")(socketServer, 
         { 
         cors: {    
-          origin: "http://52.66.244.7.nip.io:8000"
+          origin: "http://52.66.244.7.nip.io"
         }});
     // let io = require('socket.io')(socketServer);
 
