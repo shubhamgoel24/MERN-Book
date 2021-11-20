739aa6ae380fa17b45b414992609c264622c5441
Shubham Goel <sshubhamgoel24@gmail.com>
2021-11-20 22:00:26 +0530

    socket ssl

--- index.js
+++ index.js
@@ -22,8 +22,14 @@ const flash = require('connect-flash');
 const customMware = require('./config/middleware');
 const development = require('./config/environment');
 
+const fs = require('fs');
+// const https = require('https');
+
 //setup chat server to be used with socket.io
-const chatServer = require('http').Server(app);
+const chatServer = require('https').createServer({
+    key:fs.readFileSync('../server.key'),
+    cert:fs.readFileSync('../server.cert'),
+},app);
 const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
 chatServer.listen(5000);
 console.log("Chat server is listening on port 5000")
